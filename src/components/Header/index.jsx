f120c1e169d29b390697e38647434faa8da49e82
YanGraim <yangraim@gmail.com>
2024-12-19 14:13:57 -0300

    feat: cria Header e integra com rotas para renderização em todas as páginas

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <h2>TESTE HEADER</h2>
+    </header>
+  );
+}
+
+export default Header;
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import Filme from "./pages/Filme";
 import Error from "./pages/Error";
@@ -7,6 +8,7 @@ import Error from "./pages/Error";
 function Router() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/filme/:id" element={<Filme />} />
