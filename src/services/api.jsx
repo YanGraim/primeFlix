f1d24850331c3d63daa4da02ec16efd30cbae86e
YanGraim <yangraim@gmail.com>
2024-12-19 15:32:12 -0300

    feat: adiciona configuração inicial da API no arquivo api.jsx

---
+++ src/services/api.jsx
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+// BASE DA URL = https://api.themoviedb.org/3/
+// URL DA API = https://api.themoviedb.org/3/movie/now_playing?api_key=023a8520c834ea57409dc67389482981&language=pt-BR
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+});
+
+export default api;
