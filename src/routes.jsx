e4059ab870067084b583b8ba60194209bd778afc
YanGraim <yangraim@gmail.com>
2024-12-19 14:06:41 -0300

    implementa página de erro e integra ao roteamento

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,13 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Filme from "./pages/Filme";
+import Error from "./pages/Error";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/filme" element={<Filme />} />
+        <Route path="/filme/:id" element={<Filme />} />
+
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
