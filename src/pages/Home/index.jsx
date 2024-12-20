dce9fd175c565ccff4cf8ae2229442daf945abbb
YanGraim <yangraim@gmail.com>
2024-12-20 11:16:08 -0300

    feat(home): renderizar lista de filmes com título, imagem e link

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import api from "../../services/api";
+import { Link } from "react-router-dom";
 // URL DA API = movie/now_playing?api_key=023a8520c834ea57409dc67389482981&language=pt-BR
 
 function Home() {
-  const [filmes, setFilmes] = [];
+  const [filmes, setFilmes] = useState([]);
 
   useEffect(() => {
     async function loadFilmes() {
@@ -23,8 +24,21 @@ function Home() {
   }, []);
 
   return (
-    <div>
-      <h1>Página Home</h1>
+    <div className="container">
+      <div className="lista-filme">
+        {filmes.map((filme) => {
+          return (
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img
+                src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`}
+                alt={filme.title}
+              />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+          );
+        })}
+      </div>
     </div>
   );
 }
