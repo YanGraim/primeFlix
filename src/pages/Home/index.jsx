4f53d2b52db2f76c30f9d6411b166a002fb6fec3
YanGraim <yangraim@gmail.com>
2024-12-20 11:45:17 -0300

    style(home): estilizar lista de filmes na página inicial

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import api from "../../services/api";
 import { Link } from "react-router-dom";
+import "./home.css";
 // URL DA API = movie/now_playing?api_key=023a8520c834ea57409dc67389482981&language=pt-BR
 
 function Home() {
