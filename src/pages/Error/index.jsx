f1581b6e914d713b2f6cc5fb6426c0522f9b5195
YanGraim <yangraim@gmail.com>
2024-12-21 14:08:09 -0300

    style(error): estilizar a pagina de not found

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+import "./error.css";
+
 function Error() {
   return (
-    <div>
+    <div className="not-found">
+      <h1>404</h1>
       <h2>Ops, parece que essa página não existe!</h2>
+      <Link to={"/"}>Veja todos os filmes</Link>
     </div>
   );
 }
