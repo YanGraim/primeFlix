205c2682075633b523b466985636d0e291a0787e
YanGraim <yangraim@gmail.com>
2024-12-21 21:59:53 -0300

    feat(filme): adicionar chamada à API para buscar detalhes do filme

--- src/pages/Filme/index.jsx
+++ src/pages/Filme/index.jsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import api from "../../services/api";
+
 function Filme() {
+  const { id } = useParams();
+
+  useEffect(() => {
+    async function loadFilme() {
+      await api
+        .get(`/movie/${id}`, {
+          params: {
+            api_key: "023a8520c834ea57409dc67389482981",
+            language: "pt-BR",
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch(() => {
+          console.log("Filme não encontrado!");
+        });
+    }
+    loadFilme();
+  }, []);
+
   return (
     <div>
-      <h1>Página de filmes</h1>
+      <h1>Acessando filme {id}</h1>
     </div>
   );
 }
