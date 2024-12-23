ea47891baf595d6af2d7994e5966bb5e98464cda
YanGraim <yangraim@gmail.com>
2024-12-23 12:33:20 -0300

    feat(filme): adicionar função para salvar filme na lista

--- src/pages/Filme/index.jsx
+++ src/pages/Filme/index.jsx
@@ -35,6 +35,23 @@ function Filme() {
     };
   }, [navigate, id]);
 
+  function salvarFilme() {
+    const minhaLista = localStorage.getItem("@primeflix");
+    let filmesSalvos = JSON.parse(minhaLista) || [];
+
+    const hasFilme = filmesSalvos.some(
+      (filmeSalvo) => filmeSalvo.id === filme.id
+    );
+    if (hasFilme) {
+      alert("Esse filme já está na lista!");
+      return;
+    }
+
+    filmesSalvos.push(filme);
+    localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos));
+    alert("Filme salvo com sucesso!");
+  }
+
   if (loading) {
     return (
       <div className="filme-info">
@@ -55,11 +72,11 @@ function Filme() {
       <strong>Avaliação: {filme.vote_average.toFixed(1)} / 10</strong>
 
       <div className="area-buttons">
-        <button>Salvar</button>
+        <button onClick={salvarFilme}>Salvar</button>
         <button>
           <a
             href={`https://youtube.com/results?search_query=${filme.title} Trailer`}
-            target="_blank"
+            target="blank"
             rel="external"
           >
             Trailer
