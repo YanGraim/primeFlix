e8c5bb35bdba4299e7391daadeb33a44159b8e3c
YanGraim <yangraim@gmail.com>
2024-12-23 20:46:28 -0300

    feat(routes): adicionar rota para a página de favoritos

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Filme from "./pages/Filme";
+import Favoritos from "./pages/Favoritos";
 import Error from "./pages/Error";
 
 function Router() {
@@ -12,6 +13,7 @@ function Router() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/filme/:id" element={<Filme />} />
+        <Route path="/favoritos" element={<Favoritos />} />
 
         <Route path="*" element={<Error />} />
       </Routes>
