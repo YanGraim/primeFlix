d245aa66052711a865252be7960cedd4c76cfa19
YanGraim <yangraim@gmail.com>
2024-12-23 21:18:16 -0300

    feat(favoritos): exibir lista de filmes salvos na página de favoritos

--- src/pages/Favoritos/index.jsx
+++ src/pages/Favoritos/index.jsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
 import "./favoritos.css";
 
 function Favoritos() {
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(() => {
+    const minhaLista = localStorage.getItem("@primeflix");
+    setFilmes(JSON.parse(minhaLista) || []);
+  }, []);
+
   return (
-    <div>
-      <h1>Tela de favoritos</h1>
+    <div className="meus-filmes">
+      <h1>Meus filmes</h1>
+
+      <ul>
+        {filmes.map((item) => {
+          return (
+            <li key={item.id}>
+              <span>{item.title}</span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
