de7e5c5e3d19853fd4ddb6e86fc5a5095be01374
YanGraim <yangraim@gmail.com>
2024-12-23 21:24:06 -0300

    feat(favoritos): adicionar links para detalhes e botão de exclusão para cada filme

--- src/pages/Favoritos/index.jsx
+++ src/pages/Favoritos/index.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import "./favoritos.css";
 
 function Favoritos() {
@@ -12,12 +13,15 @@ function Favoritos() {
   return (
     <div className="meus-filmes">
       <h1>Meus filmes</h1>
-
       <ul>
         {filmes.map((item) => {
           return (
             <li key={item.id}>
               <span>{item.title}</span>
+              <div>
+                <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
+                <button>Excluir</button>
+              </div>
             </li>
           );
         })}
